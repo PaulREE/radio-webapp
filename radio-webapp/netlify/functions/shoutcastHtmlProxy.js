a0435447c1388daf7623e3596be0bb0893fe7ff5
PaulREE <63521243+PaulREE@users.noreply.github.com>
2025-09-25 11:18:48 +0300

    Add files via upload

--- radio-webapp/netlify/functions/shoutcastHtmlProxy.js
+++ radio-webapp/netlify/functions/shoutcastHtmlProxy.js
@@ -4,13 +4,16 @@ export const handler = async () => {
   try {
     const resp = await fetch('https://main.inf.fm:8101/index.html?sid=1', {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (NetlifyServerProxy)'
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive'
       }
     });
     const html = await resp.text();
 
-    // Поиск строки Playing Now
-    const match = html.match(/<td>Playing Now:\s*<\/td>\s*<td>.*?<b>.*?<a.*?>(.*?)<\/a>.*?<\/b>.*?<\/td>/is);
+    const pattern = /<td>Playing Now:\s*<\/td>\s*<td>.*?<b>.*?<a.*?>(.*?)<\/a>.*?<\/b>.*?<\/td>/is;
+    const match = html.match(pattern);
     const now = match ? match[1].trim() : '';
 
     let artist = '', title = '';
@@ -22,7 +25,7 @@ export const handler = async () => {
 
     return {
       statusCode: 200,
-      headers: { 
+      headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json; charset=utf-8'
       },
